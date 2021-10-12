a355b385912230b552c451a116dd71350a261d45
Laura-Johnston <laurakjgb@gmail.com>
2021-10-12 16:20:00 +0100

    added api

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,18 +1,25 @@
-
+import axios from "axios";
 import React, { useState } from "react";
 
 export default function Dictionary () {
     let [word, setWord] = useState("");
 
+    function grabDefinition(response){
+        console.log(response);
+    }
 function dictionarySearch (event){
     event.preventDefault();
     alert(`searching for ${word}`);
+
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/`
+    axios.get(apiUrl).then(grabDefinition);
 }
 function handleWordSubmit(event){
     console.log(event.target.value);
     setWord(event.target.value);
     
 }
+
 
 
 return(
